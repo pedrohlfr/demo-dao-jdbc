b32445d8447879037b5148be87a92e479bc5e7ae
Pedro Henrique <pedro2012lara@gmail.com>
2026-08-25 10:25:12 -0300

    DepartmentDao insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,12 +36,11 @@ public class Program {
 			System.out.println(item);
 		}
 		
-		/*
 		System.out.println("\n=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg","greg@gmail.com", LocalDate.now(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = "+ newSeller.getId());
-		*/
+		
 		
 		System.out.println("\n=== TEST 5: seller update ===");
 		seller = sellerDao.findById(1);
---
+++ src/application/Program2.java
@@ -0,0 +1,20 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== TEST 1: department insert ===");
+		Department department = new Department(7, "Foods");
+		departmentDao.insert(department);
+		System.out.println("Inserted! New id = "+ department.getId());
+
+	}
+
+}
